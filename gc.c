c1e049fb75be868ae8923d7f8b6ad33bd0aecfa6
Koichi Sasada <ko1@atdot.net>
2015-06-01 19:33:56 +0900

    remove garbage character

--- gc.c
+++ gc.c
@@ -4298,7 +4298,7 @@ gc_mark_children(rb_objspace_t *objspace, VALUE obj)
 	    rb_bug("T_IMEMO: unreachable");
 	}
     }
-a
+
     gc_mark(objspace, any->as.basic.klass);
 
     switch (BUILTIN_TYPE(obj)) {
